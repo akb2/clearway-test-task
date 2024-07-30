3d67cbfb20fe96ba1fa6f8496908ffbe1094267f
akb2 <akb2-online@ya.ru>
2024-07-31 03:16:27 +0400

    Список документов

--- src/app/pages/documents/documents.module.ts
+++ src/app/pages/documents/documents.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from "@angular/router";
 import { BaseLayoutModule } from "@app/components/layouts/base-layout/base-layout.module";
+import { DocumentCardModule } from "@app/components/views/document-card/document-card.module";
 import { noSubRoutes } from "@app/helpers/routes";
 import { DocumentsComponent } from './documents.component';
 
@@ -12,7 +13,8 @@ import { DocumentsComponent } from './documents.component';
   imports: [
     RouterModule.forChild(noSubRoutes(DocumentsComponent)),
     CommonModule,
-    BaseLayoutModule
+    BaseLayoutModule,
+    DocumentCardModule
   ]
 })
 export class DocumentsModule { }
