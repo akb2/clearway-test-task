cf5370717d6bdafefc3ddb4b43cbe332944b2249
akb2 <akb2-online@ya.ru>
2024-07-30 21:23:25 +0400

    Подключил стор

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { EffectsModule } from "@ngrx/effects";
+import { StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { appEffects } from "./store/app.effects";
+import { appReducer } from "./store/app.reducer";
 
 @NgModule({
   declarations: [
@@ -9,7 +14,10 @@ import { AppComponent } from "./app.component";
   ],
   imports: [
     AppRoutingModule,
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot(appReducer),
+    EffectsModule.forRoot(appEffects),
+    StoreDevtoolsModule.instrument({ maxAge: 25 })
   ],
   bootstrap: [
     AppComponent
