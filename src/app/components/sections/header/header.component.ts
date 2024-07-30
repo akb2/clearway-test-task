52c6bf7841ca431649c54dfa3b60e90fcb20a4f1
akb2 <akb2-online@ya.ru>
2024-07-31 01:28:10 +0400

    Компонент шапки

--- src/app/components/sections/header/header.component.ts
+++ src/app/components/sections/header/header.component.ts
@@ -1,10 +1,11 @@
-import { Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
-  styleUrls: ["./header.component.scss"]
+  styleUrls: ["./header.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeaderComponent {
-
+  @Input() title = "Тестовое задание";
 }
--- src/app/pages/documents/documents.component.ts
+++ src/app/pages/documents/documents.component.ts
@@ -1,9 +1,10 @@
-import { Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component } from "@angular/core";
 
 @Component({
   selector: "page-documents",
   templateUrl: "./documents.component.html",
-  styleUrls: ["./documents.component.scss"]
+  styleUrls: ["./documents.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DocumentsComponent {
 }
