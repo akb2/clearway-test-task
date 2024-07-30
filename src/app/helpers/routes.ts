57455e82fe281aa8d24cc5251546eaed62e9e7df
akb2 <akb2-online@ya.ru>
2024-07-31 01:25:23 +0400

    Описание метода

--- src/app/helpers/routes.ts
+++ src/app/helpers/routes.ts
@@ -1,4 +1,11 @@
 import { Type } from "@angular/core";
 import { Route } from "@angular/router";
 
+/**
+ * Возвращает массив маршрутов без подмаршрутов.
+ *
+ * @param component Компонент, который будет отображаться для данного маршрута.
+ * @returns Массив маршрутов без подмаршрутов.
+ * @template C Тип компонента.
+ */
 export const noSubRoutes = <C>(component: Type<C>): Route[] => ([{ path: "", component }]);
